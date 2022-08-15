9644c6cd9316f8bed4aa4054a79d87f163faf0b5
AdrienMarquer-eesmb <adrien.marquer@eiffage.com>
2022-08-15 17:24:20 +0200

    feat(): stars background

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable no-unused-vars */
+import "./App.css";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { useRef, useMemo } from "react";
+import { OrbitControls, Stars, Bounds, useBounds } from '@react-three/drei'
+
+function Planete(props) {
+  const meshRef = useRef(null);
+  useFrame(() => {
+    if (!meshRef.current) {
+      return;
+    }
+
+    meshRef.current.rotation.x += 0.01;
+    meshRef.current.rotation.y += 0.01;
+  });
+  return (
+    <mesh ref={meshRef} {...props}>
+      <sphereGeometry />
+      <meshPhongMaterial color="white" />
+    </mesh>
+  );
+}
+
+// This component wraps children in a group with a click handler
+// Clicking any object will refresh and fit bounds
+// eslint-disable-next-line react/prop-types
+function SelectToZoom({ children }) {
+  const api = useBounds()
+  return (
+    <group onClick={(e) => (e.stopPropagation(), e.delta <= 2 && api.refresh(e.object).fit())} onPointerMissed={(e) => e.button === 0 && api.refresh().fit()}>
+      {children}
+    </group>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Canvas
+      style={{
+        backgroundColor: "#111a21",
+        width: "100vw",
+        height: "100vh",
+      }}
+    >
+      <OrbitControls />
+      <ambientLight />
+      <spotLight position={[10, 15, 10]} angle={0.3}/>
+      <pointLight position={[10, 10, 10]} />
+        <Bounds fit clip observe margin={1.2}>
+          <SelectToZoom>
+            <Planete position={[-1.2, 0, 0]} />
+            <Planete position={[1.2, 0, 0]} />
+          </SelectToZoom>
+        </Bounds>
+        <Stars />
+    </Canvas>
   );
 }
 
